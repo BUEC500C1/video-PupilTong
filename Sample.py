5884a645825b589b73115f7c98327f8dc412588b
PupilTong <earth.diqiu@gmail.com>
2020-02-17 13:25:13 -0500

    fix

--- Sample.py
+++ Sample.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     #access_token_secret = str(sys.argv[4])
     keywords = str(input("input hashtags  u choosed, spilt them with ',': ")).split(',')
     directory = input("input video storage dir: ")
-    item = input("how many photos do u want: ")
+    item = int(input("how many photos do u want: "))
     t = TwitterVideoSum(consumer_key, consumer_secret, access_token, access_token_secret,keywords,directory,item)
     t.Start()
     pass
