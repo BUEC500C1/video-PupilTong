4ddde8add5cc1724b5c072dea8bd5518d34952f6
PupilTong <earth.diqiu@gmail.com>
2020-02-23 12:33:17 -0500

    update pytest

--- test_ffmpegQueue.py
+++ test_ffmpegQueue.py
@@ -1,3 +1,15 @@
 import pytest 
-def test_something():
+from TwitterVideoSum import *
+import shutil
+import importlib
+shutil.copy('key','key.py')
+from key import *
+def test():
+    #sample function
+    #consumer_key = str(sys.argv[1])
+    #consumer_secret = str(sys.argv[2])
+    #access_token = str(sys.argv[3])
+    #access_token_secret = str(sys.argv[4])
+    t = TwitterVideoSum(consumer_key, consumer_secret, access_token, access_token_secret,['cats','dogs'],'.',2)
+    t.Start()
     pass
