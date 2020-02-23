1275ce12c9701afd4594e3df74ec16d5a77ff4ca
PupilTong <earth.diqiu@gmail.com>
2020-02-23 12:26:18 -0500

    fix

--- Sample.py
+++ Sample.py
@@ -1,5 +1,6 @@
 from TwitterVideoSum import *
-from key import *
+import importlib
+import shutil
 import sys
 if __name__ == "__main__":
     #sample function
@@ -7,6 +8,8 @@ if __name__ == "__main__":
     #consumer_secret = str(sys.argv[2])
     #access_token = str(sys.argv[3])
     #access_token_secret = str(sys.argv[4])
+    shutil.copy('key','key.py')
+    from key import *
     keywords = str(input("input hashtags  u choosed, spilt them with ',': ")).split(',')
     directory = input("input video storage dir: ")
     item = int(input("how many photos do u want: "))
